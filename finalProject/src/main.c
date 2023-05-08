6b502d5be158840380450f0ab7e5d05e997d2829
Evan Cornwell <97864933+ChickenJ0hn@users.noreply.github.com>
2023-05-07 20:31:49 -0500

    Added ADC declaration
    Did some hand calculations to get the sampling rate to like 46kHz or sum. Should be good for any input audio. Will need a voltage regulated input

--- finalProject/src/main.c
+++ finalProject/src/main.c
@@ -9,10 +9,12 @@
 
 /** Includes **/
 #include <MemoryMap.h>
+#include <irq.h>
 
 #include <lowBand.h>
-#include <midBand.h>
+#include <midBand.h>d
 #include <highBand.h>
+
 
 
 
@@ -21,6 +23,8 @@
 
 
 /** Global Variables **/
+uint16_t* ADC_DMA_PING_BUFFER = (uint16_t*)0x11000;
+uint16_t* ADC_DMA_PONG_BUFFER = (uint16_t*)0x11400;
 
 
 
@@ -36,7 +40,47 @@ RVISR(IRQ_name_VECTOR, ISR_name)
 /** Main Function **/
 int main()
 {
-	// Put your code here
+
+	// First step, audio input! We can go straight from a 3.5mm aux.
+	// Take the samples and toss them thru the ADC
+
+	// we are using interrupts
+	enable_all_interrupts();
+	BIASCR = BIASEN | BGEN | (37 << BIASADJ_LSB);
+
+	// PART 1: SET UP ADC (might do function for this, we'll see)
+	// left aligned: ADCDAL = 1
+	ADC0CR = 0
+		// | ADCCIE
+		// | ADCBFIE
+		// | ADCSC 
+		| ADCDAL 
+		| ADCREF_LSB // hmm? 
+		| ADCCDIV_4
+		// | ADCRG
+		// | ADCTE
+		// | ADCTS
+		| ADCCS_HFXT
+		// | ADCDEN
+		| ADCEN
+	;
+
+	// calculated ADC0FS to be = 2
+	ADC0FS = 2;
+	// max buffer size
+	ADC0HBS = 0XFF;
+
+	// need to change stack start location???
+
+	// ADC0VAL needs to be stored in da buffers!
+
+
+
+
+
+
+
+	// output = input * (loband + midband + hiband + 1)
 
 	return 0;
 }
@@ -44,14 +88,12 @@ int main()
 
 
 /** Function Definitions **/
+uint16_t* lopass() {
+
+}
 
 
 
 /** Interrupt Service Routines **/
 
-
-void ()
-{
-
-}
 
