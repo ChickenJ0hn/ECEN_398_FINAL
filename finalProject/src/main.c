ed6ab2e37ee70061b128e351f9815408a2f07788
Evan Cornwell <97864933+ChickenJ0hn@users.noreply.github.com>
2023-05-11 16:34:09 -0500

    Audio working
    no filters yet tho but audio doo be cookin out

--- finalProject/src/main.c
+++ finalProject/src/main.c
@@ -11,15 +11,15 @@
 #include <MemoryMap.h>
 #include <irq.h>
 
-#include <lowBand.h>
-#include <midBand.h>
-#include <highBand.h>
+// #include <lowBand.h>
+// #include <midBand.h>
+// #include <highBand.h>
 
 
 
 
 /** Defines **/
-#define MAX_BUF_SIZE 127
+#define MAX_BUF_SIZE 256
 
 
 /** Global Variables **/
@@ -32,12 +32,12 @@ uint16_t* DAC_DMA_PONG_BUFFER = (uint16_t*)0x11C00;
 
 
 /** Interrupt Service Routine Declaration Macros **/
-RVISR(IRQ_name_VECTOR, ISR_name)
+// RVISR(IRQ_name_VECTOR, ISR_name)
 
 
 
 /** Function Declarations **/
-int fillDACHB(const uint16_t samples[], uint16_t length, int startIndex);
+void fillDACHB(uint16_t samples[]);
 
 
 /** Main Function **/
@@ -48,7 +48,7 @@ int main()
 	// Take the samples and toss them thru the ADC
 
 	// we are using interrupts
-	enable_all_interrupts();
+	// enable_all_interrupts();
 	BIASCR = BIASEN | BGEN | (37 << BIASADJ_LSB);
 
 
@@ -73,25 +73,39 @@ int main()
 	// calculated ADC0FS to be = 2. This gives 31250kHz sampling rate
 	ADC0FS = 2;
 	// max buffer size
-	ADC0HBS = MAX_BUF_SIZE;
+	ADC0HBS = MAX_BUF_SIZE/2 - 1;
 
 	// DAC setup!!
-	DAC->DACCR_.DACCDIV_ = 0; // DACCDIV = 0 no divider on clk src
+	DAC->DACCR_.DACCDIV_ = 2; // DACCDIV = 0 no divider on clk src
 	DAC->DACCR_.DAC0EN_ = 1; // DAC0 is enabled
 	DAC->DACCR_.DAC1EN_ = 0; // DAC1 is disabled
 	DAC->DACCR_.DAC0DEN_ = 0; // Both DMAs for DAC0 and DAC1 are disabled (for now...)
 	DAC->DACCR_.DAC1DEN_ = 0; 
 	DAC->DACCR_.DACCS_ = 2; // DACCS is selecting the HFXT clock as the DAC source clock
-	DAC->DACCR_.DACDAL_ = 0; // the DAC data alignment is right-aligned
-	DAC->DACFS_.DACFS_ = 14; // Section 19.6 calculation
+
+	// TODO: CHECK THESE TO MAKE SURE THEY LINE UP W ADC
+	DAC->DACCR_.DACDAL_ = 1; // the DAC data alignment is left-aligned!!!! ***
+	DAC->DACFS_.DACFS_ = 2; // Section 19.6 calculation
+
+	
+	DAC->DACHBS_.DACHBS_= MAX_BUF_SIZE - 1; // max DACHBS of 127 because of da ADC limitations
 
 	// ADC0VAL needs to be stored in da buffers!
+
+	// DAC DMA FILL WITH CHEERIOS (zeros)
+	for(int i = 0; i < MAX_BUF_SIZE; i++) {
+		DAC_DMA_PING_BUFFER[i] = 0;
+	}
+
+	// send audio straight through here! TURN ON ADC DEN
+	ADC0CR |= ADCDEN;
+	DAC->DACCR_.DAC0DEN_ = 1;
+
 
 	// lets get our samples, shall we?
 	while(1) {
 
-		// send audio straight through here! TURN ON ADC DEN
-		ADC0CR |= ADCDEN;
+		
 		// let it fill up and then stop the ADC from getting new samps until the next time
 		while(ADC0->SR.BFIF != 1) {}
 		// reset BFIF flag
@@ -101,19 +115,16 @@ int main()
 		// TODO: process/filter here
 
 		// outputting to da DAC
-		int index = 0;
 
-		DAC->DACCR_.DAC0DEN_ = 0;
-		DAC->DACHBS_.DACHBS_= MAX_BUF_SIZE; // max DACHBS of 127 because of da ADC limitations
-		DAC->DACCR_.DAC0DEN_ = 1;
 		while (DAC->DACSR_.DACBEIF_ == 0){}
 
 		// if the first half is being written, read from second half
 		if(ADC0->SR.BH == 0) {
-			index = fillDACHB(ADC_DMA_PONG_BUFFER, (MAX_BUF_SIZE + 1) , index);
+			fillDACHB(ADC_DMA_PONG_BUFFER);
 		}
+
 		else {
-			index = fillDACHB(ADC_DMA_PING_BUFFER, (MAX_BUF_SIZE + 1) , index);
+			fillDACHB(ADC_DMA_PING_BUFFER);
 		}
 	
 	}
@@ -129,32 +140,21 @@ int main()
 
 
 /** Function Definitions **/
-int fillDACHB(const uint16_t samples[], uint16_t length, int startIndex){
+void fillDACHB(uint16_t samples[]){
 	if (DAC->DACSR_.DACBH_ == 1){
-		for (int i = 0; i < ((DAC->DACHBS_.DACHBS_) + 1); i++){
-			DAC_DMA_PING_BUFFER[i*2] = samples[startIndex];
-			startIndex++;
-			if (startIndex >= length){
-				startIndex = 0;
-			}			
+		for (int i = 0; i < MAX_BUF_SIZE; i++){
+			DAC_DMA_PING_BUFFER[i*2] = samples[i];
+				
 		}
 	} else {
-		for (int i = 0; i < ((DAC->DACHBS_.DACHBS_) + 1); i++){
-			DAC_DMA_PONG_BUFFER[i*2] = samples[startIndex];
-			startIndex++;
-			if (startIndex >= length){
-				startIndex = 0;
-			}
+		for (int i = 0; i < MAX_BUF_SIZE; i++) {
+			DAC_DMA_PONG_BUFFER[i*2] = samples[i];
 		}
 	}
 	DAC->DACSR_.DACBEIF_ = 1; //Reset DACBEIF flag
-return startIndex;
+return;
 }
 
-
-uint16_t* lopass() {
-
-}
 
 
 
